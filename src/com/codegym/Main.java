3b3d815bc03d4d0672b6975d2b0bf1605ba1418e
unknown <qquangmminh19999@gmail.com>
2022-03-24 14:54:42 +0700

    th1

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,12 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Rectangle rectangle1 = new Rectangle(3,5);
+        System.out.println("hcn1 :" + rectangle1);
+        System.out.println("dien tich : " + rectangle1.getArea());
+        System.out.println("chu vi :" + rectangle1.getPerimeter());
+    }
+}
